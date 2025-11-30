39afe308dce69ce1c0b66e26b3346fffb17fd6ea
JDOT <JDON219206@gmail.com>
2025-11-30 20:31:40 +1100

    feat: Completed

--- UnitConverter/temp.cpp
+++ UnitConverter/temp.cpp
@@ -3,14 +3,17 @@
 #include <QMap>
 double Temp::convert(double val, QString &unit, QString &target) {
     if (unit == "Degree Celsius") {
-        if (target == "Fahrenheit") return val * 9/5 + 32;
+        if (target == "Fahrenheit") return val * (9.0/5.0) + 32.0;
         if (target == "Kelvin") return val + 273.15;
-    } else if (unit == "Fahrenheit") {
-        if (target == "Degree Celsius") return (val - 32) * (5/9);
-        if (target == "Kelvin") return (val - 32) * (5/9) + 273.15;
-    } else if (unit == "Kelvin") {
-        if (target == "Fahrenheit") return (val - 273.15) * (9/5) + 32;
+    } 
+    else if (unit == "Fahrenheit") {
+        if (target == "Degree Celsius") return (val - 32.0) * (5.0/9.0);
+        if (target == "Kelvin") return (val - 32.0) * (5.0/9.0) + 273.15;
+    } 
+    else if (unit == "Kelvin") {
+        if (target == "Fahrenheit") return (val - 273.15) * (9.0/5.0) + 32.0;
         if (target == "Degree Celsius") return val - 273.15;
     }
-    return val;
+
+    return val; 
 }
